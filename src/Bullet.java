b340607aa0090860251ba831e9417b73662c636b
LENOVOPC\Agata <agatawojtaszek96@gmail.com>
2017-05-30 02:41:39 +0200

    dodanie warunków zmiany położenia

--- src/Bullet.java
+++ src/Bullet.java
@@ -4,21 +4,20 @@ public class Bullet {
 	private int y;
 	private int stepSize=5;
 	Direction direction;
+	private int sceneHeight;
 	
 	private void Move(){
-		if(direction==Direction.Down)
-		y+=stepSize;
-		else if(direction==Direction.Up)
+		if(direction==Direction.Down && y<sceneHeight)
+			y+=stepSize;
+		else if(direction==Direction.Up && y>0)
 			y-=stepSize;
-		
 	}
 	
-	
-	
-	public Bullet(int x,int y, Direction direction){
+	public Bullet(int x,int y, Direction direction, int sceneHeight){
 		this.x=x;
 		this.y=y;
 		this.direction=direction;
+		this.sceneHeight=sceneHeight;
 	}
 
 }
